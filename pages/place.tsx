5fa61fe9a06a5f6b6e9267ec6a97d5bc98695f7b
DokuaAsiedu <asiedudokua@gmail.com>
2024-04-29 03:30:55 +0000

    added dummy data for opening hours and days and changed layout of location page to display them

--- pages/place.tsx
+++ pages/place.tsx
@@ -11,7 +11,7 @@ export default function Place() {
     const { id } = router.query;
 
     const match = places.find((item) => item.id === Number(id));
-    // console.log(match)
+    console.log(match)
     return match;
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [router.query.id, places]);
@@ -20,13 +20,21 @@ export default function Place() {
     <GeneralLayout>
       <div className="container">
         {placeData && (
-          <>
-            <p>
-              <span className="capitalize font-bold">{placeData.name}</span>,{" "}
-              {placeData.location}
-            </p>
+          <div className="flex flex-col gap-2">
+            <div className="grid grid-cols-1 md:grid-cols-2">
+              <p className="capitalize font-bold">{placeData.name}</p>
+              <p className="md:justify-self-end capitalize font-bold">{placeData.location}</p>
+            </div>
+            <div>{placeData.openPeriods.map((item, index) => {
+              return (
+                <div key={`item-${index}`} className="grid grid-cols-2 font-bold">
+                  <p className="capitalize">{item.days.join(', ')}</p>
+                  <p className="justify-self-end self-center">{`${item.start}:${item.end}`}</p>
+                </div>
+              )
+            })}</div>
             <p>{placeData.description}</p>
-          </>
+          </div>
         )}
       </div>
     </GeneralLayout>
